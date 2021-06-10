1d318191fe9891095506d4693515e83d89773601
L7z404 <leonelcastillo1014@gmail.com>
2021-06-09 22:50:27 -0500

    Encabezado terminado

--- index.php
+++ index.php
@@ -5,9 +5,32 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/vac_style.css" type="text/css">
     <title>Mi Vacuna</title>
 </head>
 <body>
+    <div id="all_cabeza">
+        <div id="raya_alta_cabeza"></div>
+        <div id="centro_cabeza">
+            <div id="imgDiv">
+
+            </div>
+        </div>
+        <div id="pie_cabeza"></div>
+        <div id="raya_baja_cabeza"></div>
+    </div>
+    <br>
+    <br>
+    <br>
+
+    <div id="container_dato_curp">
+        <div id="info_a_curp">
+
+        </div>
+        <div id="curp">
+
+        </div>
+    </div>
 
 </body>
 </html>
