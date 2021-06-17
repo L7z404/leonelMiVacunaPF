d0ed3722711317a75418cc9a47c75bbef45a64a8
L7z404 <leonelcastillo1014@gmail.com>
2021-06-16 20:40:03 -0500

    Botones ahora tienen enlace y agregado el favicon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="images/favicon.png">
     <link rel="stylesheet" href="css/vac_style.css" type="text/css">
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <title>Mi Vacuna</title>
@@ -52,13 +53,13 @@
     </form>
     <br><br><br>
     <div id="botones_abajo_index">
-        <button id="bregmivac" type="button">Guía Para Registrarse en mivacuna</button>
+        <button id="bregmivac" type="button"><a style="text-decoration: none; color: #fff" href="https://mivacuna.salud.gob.mx/pdf/registro_vacuna_imgns_c6.pdf" target="_blank">Guía Para Registrarse en mivacuna</a></button>
         &ensp;&ensp;
-        <button id="bnoconoce" type="button">¿No conoces tu CURP...? Consúltala aquí</button>
+        <button id="bnoconoce" type="button"><a style="text-decoration: none; color: #fff" href="https://www.gob.mx/curp/" target="_blank">¿No conoces tu CURP...? Consúltala aquí</a></button>
         &ensp;&ensp;
-        <button id="baviso" type="button">Aviso de Privacidad</button>
+        <button id="baviso" type="button"><a style="text-decoration: none; color: #fff" href="https://mivacuna.salud.gob.mx/pdf/aviso_de_privacidad_integral.pdf" target="_blank">Aviso de Privacidad</a></button>
         &ensp;&ensp;
-        <button id="infodosis" type="button">Información segunda dosis</button>
+        <button id="infodosis" type="button"><a style="text-decoration: none; color: #fff" href="https://mivacuna.salud.gob.mx/pdf/info_segunda_dosis.pdf" target="_blank">Información segunda dosis</a></button>
 
     </div>
     <br><br><br>
