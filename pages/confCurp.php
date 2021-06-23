e592e1b665a6657f3e055e910a614229f84aab70
L7z404 <leonelcastillo1014@gmail.com>
2021-06-23 15:38:35 -0500

    Agregando mas contenido a confCurp

--- pages/confCurp.php
+++ pages/confCurp.php
@@ -35,11 +35,69 @@
     <br><br><br>
     <form id="confCenterForm" name="confCurpForm">
         <div id="configCurp">
-            <div id="datosConfigCurp">asdfasdfasdfasdfasdfadsfasdfasdfads</div>
+            <div id="info_a_curp">Confirmación de CURP</div>
+
+            <div id="datosPersonaCurp">
+
+                <div id="espacioDatos" style="margin-top: 1em"></div>
+                <label for="txtNombre"><strong>Nombre:</strong>
+                    <input id="txtNombre" placeholder="Primer Nombre" style="width: 15%" />
+                </label>
+
+                <label for="txtApellidoP"><strong>Apellido Paterno:</strong>
+                    <input id="txtApellidoP" placeholder="Apellido Paterno" style="width: 15%" />
+                </label>
+
+                <label for="txtApellidoM"><strong>Apellido Materno:</strong>
+                    <input id="txtApellidoM" placeholder="Apellido Materno" style="width: 15%" />
+                </label>
+
+                <br>
+                <label for="txtCurp"><strong>CURP:</strong>
+                    <input id="txtCurp" placeholder="CURP" style="width: 15%" />
+                </label>
+                <label for="txtFechaNac"><strong>Fecha Nacimiento:</strong>
+                    <input id="txtFechaNac" placeholder="xx/xx/xxxx" style="width: 15%" />
+                </label>
+                <label for="txtEntidadNac"><strong>Entidad Nacimiento:</strong>
+                    <input id="txtEntidadNac" placeholder="Entidad Nacimiento" style="width: 15%" />
+                </label>
+                <label for="txtSexo"><strong>Sexo:</strong>
+                    <input id="txtSexo" placeholder="Sexo" style="width: 15%" />
+                </label>
+
+                <br><br>
+                <label for="comboPostracion"><i>¿Se encuentra en estado de postración?</i></label>
+                <select id="comboPostracion">
+                    <option selected disabled>--</option>
+                    <option>Sí</option>
+                    <option>No</option>
+                </select>
+                &ensp; &ensp; &ensp; &ensp;
+                <label for="comboDiabetes"><i>¿Padece diabetes?</i></label>
+                <select id="comboDiabetes">
+                    <option selected disabled>--</option>
+                    <option>Sí</option>
+                    <option>No</option>
+                </select>
+                &ensp; &ensp; &ensp; &ensp;
+                <label for="comboHiperten"><i>¿Padece hipertensión?</i></label>
+                <select id="comboHiperten">
+                    <option selected disabled>--</option>
+                    <option>Sí</option>
+                    <option>No</option>
+                </select>
+            </div>
+            <br>
             <div id="textoConfigCurp" >
                 <p>Se garantiza la protección de los datos personales en cumplimiento con la Ley General de Protección de Datos Personales en Posesión de Sujetos Obligados. Los datos personales y sensibles serán utilizados y vinculados para verificación y confirmación de la identidad dentro del marco de la planeación, implementación y aplicación de la Política Nacional de Vacunación y demás políticas sociales, así como para integrar expedientes y bases de datos necesarias para, en su caso, el otorgamiento y operación de políticas sociales del Gobierno Federal, así como las obligaciones que se deriven de estos y para mantener una base histórica con fines estadísticos y de obligaciones relativas a la transparencia, en términos de la normatividad y disposiciones aplicables. Consulte el aviso integral de privacidad en https://mivacuna.salud.gob.mx Lo anterior se informa en cumplimiento a los artículos 26, 27 y 28 de la Ley General de Protección de Datos Personales en Posesión de Sujetos Obligados. La Política Nacional de Vacunación es de carácter público, ajeno a cualquier partido político. Queda prohibido su uso para fines distintos a los establecidos.</p>
             </div>
-            <div id="botonesConfigCurp"></div>
+            <div id="botonesConfigCurp">
+                <button id="botonVacu" type="button" onclick=""><strong>Quiero Vacunarme</strong></button>
+                <br><br>
+                <button id="botonRegresar" type="button" onclick="window.location.href='../index.php'"><strong>Regresar</strong></button>
+            </div>
+            <div id="espacioDatos" style="margin-bottom: 1em"></div>
         </div>
     </form>
 
